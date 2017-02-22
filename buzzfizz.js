616d7e68cee6d49e921989dce182636d8301281c
Sujan Pant <name>
2017-02-22 15:46:14 -0600

    added file

---
+++ buzzfizz.js
@@ -0,0 +1,26 @@
+
+function fizz_buzz(){
+    for(i=1; i <= 100; i++){
+      if(i%3 === 0 && i%5 ==0){
+        console.log("BUZZFIZZ")
+        continue;
+      }
+        if(i%3 === 0){
+          console.log("FIZZ")
+          continue;
+        }
+        if(i%5 === 0){
+          console.log("buzz")
+          continue;
+        }
+
+        console.log(i);
+
+    }
+}
+fizz_buzz();
+
+//hello
+//the fha nojlj
+fadfads 
+//hjjhjh
